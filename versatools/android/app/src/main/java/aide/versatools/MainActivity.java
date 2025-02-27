9e1ee4702952437b0045c5ff5402b5153cac5be5
Pratik Patil <connect@pratikpatil.net>
2025-02-27 19:25:54 +0530

    Base project, android and icon

---
+++ versatools/android/app/src/main/java/aide/versatools/MainActivity.java
@@ -0,0 +1,5 @@
+package aide.versatools;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ versatools/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'aide.versatools',
+  appName: 'versatools',
+  webDir: 'www'
+};
+
+export default config;
